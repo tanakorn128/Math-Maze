cb494a03b91d8fb7296dbf7d6c136af9aae8832a
Thanakorn <60030041@lmitl.ac.th>
2020-12-13 15:45:54 +0700

    player

--- Math-Maze/script/player.gd
+++ Math-Maze/script/player.gd
@@ -1,8 +1,30 @@
 extends KinematicBody2D
-var velocuty = Vector2()
-var speed = 500
-func _process(delta):
+
+var velocity = Vector2()
+
+var Global
+var speed = 3000;
+func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
-		velocuty.x
-	
+		velocity.y = 0
+		velocity.x = -speed
+		if speed > 0: $AnimatedSprite.play("left")
+	elif Input.is_action_pressed("ui_right"):
+		velocity.y = 0
+		velocity.x = speed
+		if speed > 0: $AnimatedSprite.play("right")
+	elif Input.is_action_pressed("ui_up"):
+		velocity.x = 0
+		velocity.y=-speed
+		if speed > 0: $AnimatedSprite.play("up")
+	elif Input.is_action_pressed("ui_down"):
+		if speed > 0: $AnimatedSprite.play("down")
+		velocity.x = 0
+		velocity.y=+speed
+		
+	else:
+		velocity.y=0
+		velocity.x=0
+		$AnimatedSprite.play("idle")
+	move_and_slide(velocity*delta)
 	pass
